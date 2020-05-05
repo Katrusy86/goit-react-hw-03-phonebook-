aca8fe0d0cf90bca423d45667f70a552abe2f380
Katrusy86 <katrin.myronova@gmail.com>
2020-05-05 18:47:29 +0300

    Updates

---
+++ precache-manifest.ccf3e01f819e8e0b43e1d19f2ce19f2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d62e9e0b563f8c2e00b35dd3a882544",
+    "url": "/goit-react-hw-03-phonebook-/index.html"
+  },
+  {
+    "revision": "69d322e2da1cccbba9e5",
+    "url": "/goit-react-hw-03-phonebook-/static/css/main.e74a3145.chunk.css"
+  },
+  {
+    "revision": "ccf69dc028dc4a087d88",
+    "url": "/goit-react-hw-03-phonebook-/static/js/2.5839db50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook-/static/js/2.5839db50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69d322e2da1cccbba9e5",
+    "url": "/goit-react-hw-03-phonebook-/static/js/main.d6a44115.chunk.js"
+  },
+  {
+    "revision": "678730d25ccdb1c599f0",
+    "url": "/goit-react-hw-03-phonebook-/static/js/runtime-main.21a5561e.js"
+  }
+]);
